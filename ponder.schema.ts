24478665adc059cf40a2339c920bb8a8c548736a
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-09-17 22:14:21 -0400

    add schema

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,8 +1,21 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  Example: p.createTable({
-    id: p.string(),
-    name: p.string().optional(),
+  Round: p.createTable({
+    id: p.bigint(),
+    question: p.string().optional(),
+    submissionDeadline: p.bigint(),
+    potAmount: p.bigint(),
+    feeAmount: p.bigint(),
+    correctAnswer: p.bigint(),
+    winningAnswer: p.bigint(),
+    winners: p.hex().list(),
+    isFinalized: p.boolean(),
+  }),
+  Submission: p.createTable({
+    id: p.hex(),
+    round: p.bigint(),
+    submitter: p.hex(),
+    entry: p.bigint(),
   }),
 }));
