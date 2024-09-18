de38131462c416202b591fe9b652027955a2e834
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-09-17 22:14:49 -0400

    add indexing functions

--- src/Jellybeans.ts
+++ src/Jellybeans.ts
@@ -1,17 +1,54 @@
 import { ponder } from "@/generated";
 
-ponder.on("Jellybeans:FeesWithdrawn", async ({ event, context }) => {
-  console.log(event.args);
+ponder.on("Jellybeans:RoundInitialized", async ({ event, context }) => {
+  console.log("Jellybeans:RoundInitialized");
+
+  const { Round } = context.db;
+
+  await Round.create({
+    id: event.args.roundId,
+    data: {
+      question: event.args.question,
+      submissionDeadline: event.args.submissionDeadline,
+      potAmount: event.args.potAmount,
+      feeAmount: event.args.feeAmount,
+      correctAnswer: 0n,
+      winningAnswer: 0n,
+      winners: [],
+      isFinalized: false,
+    },
+  });
 });
 
 ponder.on("Jellybeans:GuessSubmitted", async ({ event, context }) => {
-  console.log(event.args);
+  console.log("Jellybeans:GuessSubmitted");
+
+  const { Submission } = context.db;
+
+  await Submission.create({
+    id: event.transaction.hash,
+    data: {
+      round: event.args.roundId,
+      submitter: event.args.submitter,
+      entry: event.args.guess,
+    },
+  });
 });
 
-ponder.on("Jellybeans:RoleAdminChanged", async ({ event, context }) => {
-  console.log(event.args);
-});
+ponder.on("Jellybeans:WinnerSelected", async ({ event, context }) => {
+  console.log("Jellybeans:WinnerSelected");
 
-ponder.on("Jellybeans:RoleGranted", async ({ event, context }) => {
-  console.log(event.args);
+  const { Round } = context.db;
+
+  Round.update({
+    id: event.args.roundId,
+    data: {
+      correctAnswer: event.args.correctAnswer,
+      winningAnswer: event.args.winners.reduce((a, b) =>
+        a.entry > b.entry ? a : b
+      ).entry,
+      winners: event.args.winners.map((submission) => submission.submitter),
+      isFinalized: true,
+    },
+  });
 });
